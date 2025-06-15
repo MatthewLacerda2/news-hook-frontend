aa1dd5f38e39e201c025fa79863f4386d12a7609
Matthew Lacerda <math2.0@hotmail.com>
2025-06-15 14:58:27 -0300

    fix home header mobile

--- src/components/home-header.tsx
+++ src/components/home-header.tsx
@@ -10,7 +10,7 @@ export default function HomeHeader({ token }: { token: string | undefined }) {
     <nav className="bg-gray-950 border-b border-white/10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <div className="hidden md:flex flex-1 justify-center space-x-18">
+          <div className="flex-1 justify-center space-x-18">
             <Link href="/pricing" className={tabStyle}>
               Pricing
             </Link>
